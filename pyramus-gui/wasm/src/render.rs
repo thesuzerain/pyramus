e2bfc81e9e8fd8d9ab60403cbeea922bf29547f6
Wyatt Verchere <wverchere@gmail.com>
2024-02-08 15:38:36 -0800

    thiserror (#4)

--- pyramus-gui/wasm/src/render.rs
+++ pyramus-gui/wasm/src/render.rs
@@ -2,11 +2,11 @@ use pyramus::models::example_stage;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(js_name = testRenderResvg)]
-pub fn test_render(canvas: &web_sys::HtmlCanvasElement) -> Result<(), JsValue> {
-    pyramus::render::render(&example_stage(), canvas)
+pub fn test_render(canvas: &web_sys::HtmlCanvasElement) -> Result<(), JsError> {
+    Ok(pyramus::render::render(&example_stage()?, canvas)?)
 }
 
 #[wasm_bindgen(js_name = testRenderStringResvg)]
-pub fn test_render_string() -> Result<String, JsValue> {
-    pyramus::render::render_string(&example_stage())
+pub fn test_render_string() -> Result<String, JsError> {
+    Ok(pyramus::render::render_string(&example_stage()?)?)
 }
--- pyramus/src/lib.rs
+++ pyramus/src/lib.rs
@@ -1,3 +1,46 @@
+use std::sync::PoisonError;
+
+use thiserror::Error;
+use wasm_bindgen::JsValue;
+
 pub mod log;
 pub mod models;
 pub mod render;
+
+pub type Result<T> = std::result::Result<T, PyramusError>;
+
+#[derive(Error, Debug)]
+pub enum PyramusError {
+    #[error("USVG error: {0}")]
+    USVGError(#[from] resvg::usvg::Error),
+
+    #[error("Svg types error: {0}")]
+    SvgTypesError(#[from] svgtypes::Error),
+
+    #[error("Std error: {0}")]
+    StdError(#[from] std::io::Error),
+
+    #[error("Thread poison error")]
+    PoisonError,
+
+    #[error("JsValue error: {0}")]
+    JsValue(String),
+
+    #[error("Invalid size: {0}, {1}")]
+    InvalidSize(f32, f32),
+
+    #[error("Other error: {0}")]
+    OtherError(String),
+}
+
+impl<T> From<PoisonError<T>> for PyramusError {
+    fn from(_: PoisonError<T>) -> Self {
+        PyramusError::PoisonError
+    }
+}
+
+impl From<JsValue> for PyramusError {
+    fn from(value: JsValue) -> Self {
+        PyramusError::JsValue(value.as_string().unwrap_or_default())
+    }
+}
